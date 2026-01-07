f651280ae8ad7351e2384848e9d7357ef3e7fd2f
gurman <pannug598@gmail.com>
2026-01-06 18:27:01 -0800

    Updated Main.js

--- Lab 0/js/Game.js
+++ Lab 0/js/Game.js
@@ -3,32 +3,41 @@ class Game {
     constructor() {
         this.arrayOfColors = ["Red", "Orange", "Yellow", "Green", "Blue", "Purple", "Pink"];
         this.arrayButtons = [];
-        console.log("Constructor Called")
+        console.log("Constructor Called") //REMOVE
     }
 
     createButtons(numOfButtons) {
         this.arrayButtons = [];
-        console.log("Func called")
+        console.log("Func called") //REMOVE
         console.log(numOfButtons)
         for (let index = 0; index < numOfButtons; index++) {
-            arrayButtons.push(new Button(
-                arrayOfColors[index],
+            this.arrayButtons.push(new Button(
+                this.arrayOfColors[index],
                 "100px",
                 "100px",
                 "0px",
                 "0px",
                 index
             ))
-            console.log("Button Created")
-            // arrayButtons[index] = new Button(
-            //     arrayOfColors[index],
-            //     "100px",
-            //     "100px",
-            //     "0px",
-            //     "0px",
-            //     index
-            // )
+            console.log("Button Created") //REMOVE
         }
-        console.log(this.arrayButtons)
+        console.log(this.arrayButtons) //REMOVE
+    }
+
+    setRandomPositions() {
+        this.arrayButtons.forEach(element => {
+            element.setLocation(
+                Math.floor(Math.random() * 100) + "px",
+                Math.floor(Math.random() * 100) + "px"
+            )
+            console.log("Set random values for button") //REMOVE
+        });
+    }
+
+    drawButtons() {
+        this.arrayButtons.forEach(button => {
+            button = document.createElement("button")
+            
+        });
     }
 }
--- Lab 0/js/main.js
+++ Lab 0/js/main.js
@@ -1,5 +1,10 @@
 function startGame() {
     const game = new Game()
-    const numOfButtons = (document.getElementById("buttonNum"))
+    const numOfButtons = document.getElementById("buttonNum").value
     game.createButtons(numOfButtons)
+    game.drawButtons()
+    // Wait numOfButtons seconds
+    game.setRandomPositions()
+    game.drawButtons()
+
 }
