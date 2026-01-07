11e421080e6c015396c4c1d59f42467cea2e888c
XGurmanX <pannug598@gmail.com>
2026-01-06 19:52:54 -0800

    Created 2 functions to have two ways to draw the buttons

--- Lab 0/js/Game.js
+++ Lab 0/js/Game.js
@@ -13,10 +13,10 @@ class Game {
         for (let index = 0; index < numOfButtons; index++) {
             this.arrayButtons.push(new Button(
                 this.arrayOfColors[index],
+                "10em",
+                "5em",
                 "100px",
                 "100px",
-                "0px",
-                "0px",
                 index
             ))
             console.log("Button Created") //REMOVE
@@ -27,16 +27,52 @@ class Game {
     setRandomPositions() {
         this.arrayButtons.forEach(element => {
             element.setLocation(
-                Math.floor(Math.random() * 100) + "px",
-                Math.floor(Math.random() * 100) + "px"
+                Math.floor(Math.random() * 10) + "em",
+                Math.floor(Math.random() * 10) + "em"
             )
             console.log("Set random values for button") //REMOVE
         });
     }
 
-    drawButtons() {
+    drawButtons1() {
+        console.log("Draw Buttons Called") //REMOVE
+
+        const buttonContainer = document.getElementById("ButtonContainer")
+        buttonContainer.innerHTML = ""
+
         this.arrayButtons.forEach(button => {
-            button = document.createElement("button")
+            var element = document.createElement("button")
+
+            element.style.position = "static"
+            element.style.width = button.width
+            element.style.height = button.height
+            element.style.backgroundColor = button.color
+            element.textContent = button.order + 1
+
+            buttonContainer.appendChild(element)
+            
+        });
+    }
+
+        drawButtons2() {
+        console.log("Draw Buttons Called") //REMOVE
+
+        const buttonContainer = document.getElementById("ButtonContainer")
+        buttonContainer.innerHTML = ""
+
+        this.arrayButtons.forEach(button => {
+            var element = document.createElement("button")
+
+            element.style.position = "absolute"
+            element.style.width = button.width
+            element.style.height = button.height
+            element.style.backgroundColor = button.color
+            element.style.top = button.top
+            element.style.left = button.left
+            element.style.zIndex = button.order
+            element.textContent = button.order + 1
+
+            buttonContainer.appendChild(element)
             
         });
     }
--- Lab 0/js/main.js
+++ Lab 0/js/main.js
@@ -1,10 +1,17 @@
 function startGame() {
     const game = new Game()
     const numOfButtons = document.getElementById("buttonNum").value
-    game.createButtons(numOfButtons)
-    game.drawButtons()
-    // Wait numOfButtons seconds
-    game.setRandomPositions()
-    game.drawButtons()
+    if (numOfButtons >= 4 && numOfButtons <= 7) {
+        game.createButtons(numOfButtons)
+        game.drawButtons1()
+        setTimeout(() => {
+            console.log("This runs after " + numOfButtons + " seconds.");
+            game.setRandomPositions()
+            game.drawButtons2()
+        }, numOfButtons * 1000);
 
+    } else {
+        alert("Please enter a number between 4 and 7")
+    }
 }
+
