733b20aa6141502fd0d5e7e17b4953412443bc4b
XGurmanX <pannug598@gmail.com>
2026-01-09 21:13:35 -0800

    Add one for default entry

--- server.js
+++ server.js
@@ -34,6 +34,7 @@ const PORT = process.env.PORT || 8080;
 
 app.use("/Lab0", express.static(path.join(process.cwd(), "Labs/Lab 0")));
 app.use("/LabTest", express.static(path.join(process.cwd(), "Labs/LabTest")));
+app.use("/", express.static(path.join(process.cwd(), "Labs/LabTest")));
 
 app.listen(PORT, "0.0.0.0", () => {
   console.log(`Server running on port ${PORT}`);
