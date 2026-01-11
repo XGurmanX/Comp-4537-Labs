a7b8af20c54245427b1bb34e61e3e7ec58eec62e
XGurmanX <pannug598@gmail.com>
2026-01-11 12:42:39 -0800

    Set position according to the screen size

--- Labs/Lab 0/js/Button.js
+++ Labs/Lab 0/js/Button.js
@@ -9,9 +9,13 @@ class Button {
         this.order = order;
     }
 
+    /**
+     * Sets random position for the button within the viewport
+     * Equation gives back 20-80% of the viewport dimensions
+     */
     setRandomPosition() {
-        this.top = Math.floor(Math.random() * 500) + "px";
-        this.left = Math.floor(Math.random() * 500) + "px";
+        this.top = Math.floor((Math.random() * (window.innerHeight - window.innerHeight * 0.4)) + window.innerHeight * 0.2) + "px";
+        this.left = Math.floor((Math.random() * (window.innerWidth - window.innerWidth * 0.4)) + window.innerWidth * 0.0) + "px";
     }
 
 }
