be0abdc62b00630d2da373a6f9fc1b64b0fbcbad
XGurmanX <pannug598@gmail.com>
2026-01-12 22:38:52 -0800

    push

---
+++ Labs/Lab 1/js/script.js
@@ -0,0 +1,10 @@
+function testReader() {
+    console.log("Reader Function Activated")
+    const myInterval = setInterval(() => {
+        console.log("Reader Function Activated")
+    }, 2000);
+}
+
+function testWriter() {
+    console.log("Writer Function Activated")
+}
